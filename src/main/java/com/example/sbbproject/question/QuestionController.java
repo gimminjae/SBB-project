08a6f2daafe897310cb4d6d82254575eef9169d9
gimminjae <min356812@naver.com>
2022-10-01 18:22:21 +0900

    question_form 생성, url 매핑

--- src/main/java/com/example/sbbproject/question/QuestionController.java
+++ src/main/java/com/example/sbbproject/question/QuestionController.java
@@ -5,6 +5,7 @@ import com.example.sbbproject.answer.AnswerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,5 +34,9 @@ public class QuestionController {
         model.addAttribute("question", questionDto);
         model.addAttribute("answerList", answerDtoList);
         return "question_detail";
+    }
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
     }
 }
