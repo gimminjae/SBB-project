bf6b5fda9870b26080af2425b9bb8e2abdef7f53
gimminjae <min356812@naver.com>
2022-10-01 17:19:52 +0900

    답변 등록 및 리스팅 구현

--- src/main/java/com/example/sbbproject/UtilDto.java
+++ src/main/java/com/example/sbbproject/UtilDto.java
@@ -1,5 +1,7 @@
 package com.example.sbbproject;
 
+import com.example.sbbproject.answer.Answer;
+import com.example.sbbproject.answer.AnswerDto;
 import com.example.sbbproject.question.Question;
 import com.example.sbbproject.question.QuestionDto;
 
@@ -7,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UtilDto {
-    public static List<QuestionDto> toDtoList(List<Question> questionList) {
+    public static List<QuestionDto> toQuestionDtoList(List<Question> questionList) {
         List<QuestionDto> questionDtoList = new ArrayList<>();
         for(Question question : questionList) {
             questionDtoList.add(new QuestionDto(question.getId(), question.getSubject(), question.getContent(), question.getCreateDate()));
@@ -16,5 +18,15 @@ public class UtilDto {
     }
     public static QuestionDto toDto(Question question) {
         return question.toDto();
+    }
+    public static List<AnswerDto> toAnswerDtoList(List<Answer> answerList) {
+        List<AnswerDto> answerDtoList = new ArrayList<>();
+        for(Answer answer : answerList) {
+            answerDtoList.add(new AnswerDto(answer.getId(), answer.getContent(), answer.getCreateDate()));
+        }
+        return answerDtoList;
+    }
+    public static AnswerDto toDto(Answer answer) {
+        return answer.toDto();
     }
 }
--- src/main/java/com/example/sbbproject/question/QuestionController.java
+++ src/main/java/com/example/sbbproject/question/QuestionController.java
@@ -1,6 +1,7 @@
 package com.example.sbbproject.question;
 
 import com.example.sbbproject.UtilDto;
+import com.example.sbbproject.answer.AnswerDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,14 +20,18 @@ public class QuestionController {
     @RequestMapping("/list")
     public String questionList(Model model) {
         List<Question> questionList = questionService.getAll();
-        List<QuestionDto> questionDtoList = UtilDto.toDtoList(questionList);
+        List<QuestionDto> questionDtoList = UtilDto.toQuestionDtoList(questionList);
         model.addAttribute("questionList", questionDtoList);
         return "question_list";
     }
     @RequestMapping("/detail/{questionId}")
     public String questionDetail(Model model, @PathVariable(value = "questionId") Long questionId) {
-        QuestionDto questionDto = UtilDto.toDto(questionService.getQuestionById(questionId));
+        Question question = questionService.getQuestionById(questionId);
+        QuestionDto questionDto = UtilDto.toDto(question);
+        List<AnswerDto> answerDtoList = UtilDto.toAnswerDtoList(question.getAnswerList());
+
         model.addAttribute("question", questionDto);
+        model.addAttribute("answerList", answerDtoList);
         return "question_detail";
     }
 }
